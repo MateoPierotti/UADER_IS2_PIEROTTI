fb9a50be3b85a32268af508f319e84535501f131
MateoPierotti <mateopierotti@gmail.com>
2024-03-20 17:42:00 -0300

    factorial entre ambos extremos

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -20,11 +20,18 @@ def factorial(num):
             num -= 1
         return fact 
 
-if len(sys.argv) == 1:
-   print("Debe informar un número!")
-   sys.exit()
+if len(sys.argv) != 3:
+    print("Debe informar dos números para formar un rango.")
+    sys.exit()
 else:
-    num = int(sys.argv[1])
-    print("Factorial ",num,"! es ", factorial(num)) 
+    start = int(sys.argv[1])
+    end = int(sys.argv[2])
 
+    if start > end:
+        print("El primer número debe ser menor o igual que el segundo número.")
+        sys.exit()
+    
+    print("Factoriales dentro del rango", start, "-", end, ":")
+    for num in range(start, end + 1):
+        print("Factorial de", num, "es", factorial(num))
 
